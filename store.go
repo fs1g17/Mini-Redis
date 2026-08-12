958d6bab0b8921bf31a6c1fed0003c13885cfe51
fs1g17 <fs1g17@soton.ac.uk>
2026-08-12 16:06:16 +0400

    feat: add store

---
+++ store.go
@@ -0,0 +1,53 @@
+package main
+
+import "sync"
+
+type Store interface {
+	Set(key string, value string) error
+	Get(key string) (string, error)
+	Del(key string) error
+	Exists(key string) (bool, error)
+}
+
+type RedisStore struct {
+	mu   sync.Mutex
+	data map[string]string
+}
+
+func NewRedisStore() *RedisStore {
+	return &RedisStore{
+		data: make(map[string]string, 512),
+	}
+}
+
+func (r *RedisStore) Set(key string, value string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.data[key] = value
+	return nil
+}
+
+func (r *RedisStore) Get(key string) (string, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	return r.data[key], nil
+}
+
+func (r *RedisStore) Del(key string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	delete(r.data, key)
+
+	return nil
+}
+
+func (r *RedisStore) Exists(key string) (bool, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	_, ok := r.data[key]
+	return ok, nil
+}
